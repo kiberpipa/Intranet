958e982a939aa8bc52c1e4d6a2910baabc18d428
Almir Karic <almir@kiberpipa.org>
2008-08-12 16:26:31 +0000

    hekau input za dogodke (mamo kul input widget + zdej length dela)
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@924 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- intranet/org/forms.py
+++ intranet/org/forms.py
@@ -6,8 +6,80 @@ from intranet.org.models import Bug, Resolution, Clipping, Task, Project
 from intranet.org.models import Category
 
 
+# -*- coding: utf-8 -*-
+# widgets.py
+#
+# To use you have to put calendar/ (from http://www.dynarch.com/projects/calendar/)
+# to your MEDIA folder and then include such links on your page:
+# <!-- calendar -->
+# <link rel="stylesheet" type="text/css" href="{{ MEDIA_URL }}calendar/calendar-win2k-cold-2.css" />
+#<script type="text/javascript" src="{{ MEDIA_URL }}calendar/calendar.js"></script>
+# <!-- this is translation file - choose your language here -->
+#<script type="text/javascript" src="{{ MEDIA_URL }}calendar/lang/calendar-pl.js"></script>
+#<script type="text/javascript" src="{{ MEDIA_URL }}calendar/calendar-setup.js"></script>
+#<!-- /calendar -->
+
+from django.utils.encoding import force_unicode
+from django.conf import settings
+from django import newforms as forms
+#from datetime import datetime, date
+import datetime
+import time
+
+# DATETIMEWIDGET
+calbtn = u"""<img src="http://www.up-rs.si/up-rs/uprs.nsf/calendar.jpg" alt="calendar" id="%s_btn" style="cursor: pointer; border: 1px solid #8888aa;" title="Select date and time"
+            onmouseover="this.style.background='#444444';" onmouseout="this.style.background=''" />
+<script type="text/javascript">
+    Calendar.setup({
+        inputField     :    "%s",
+        ifFormat       :    "%s",
+        button         :    "%s_btn",
+        singleClick    :    true,
+        showsTime      :    true
+    });
+</script>"""
+
+class DateTimeWidget(forms.widgets.TextInput):
+    dformat = '%Y-%m-%d %H:%M'
+    def render(self, name, value, attrs=None):
+        if value is None: value = ''
+        final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
+        if value != '': 
+            try:
+                final_attrs['value'] = \
+                                   force_unicode(value.strftime(self.dformat))
+            except:
+                final_attrs['value'] = \
+                                   force_unicode(value)
+        if not final_attrs.has_key('id'):
+            final_attrs['id'] = u'%s_id' % (name)
+        id = final_attrs['id']
+        
+        jsdformat = self.dformat #.replace('%', '%%')
+        cal = calbtn % (id, id, jsdformat, id)
+        a = u'<input%s />%s' % (forms.util.flatatt(final_attrs), cal)
+        return a
+
+    def value_from_datadict(self, data, files, name):
+        dtf = forms.fields.DEFAULT_DATETIME_INPUT_FORMATS
+        empty_values = forms.fields.EMPTY_VALUES
+
+        value = data.get(name, None)
+        if value in empty_values:
+            return None
+        if isinstance(value, datetime.datetime):
+            return value
+        if isinstance(value, datetime.date):
+            return datetime(value.year, value.month, value.day)
+        for format in dtf:
+            try:
+                return datetime.datetime(*(time.strptime(value, format))[:6])
+            except ValueError:
+                continue
+        return None
 
 class EventForm(forms.ModelForm):
+    start_date = forms.DateTimeField(widget=DateTimeWidget)
     class Meta:
         model = Event
 
