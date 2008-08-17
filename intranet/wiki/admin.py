44d778786f2e8e83b6e493731ead361c71b4d6b0
Almir Karic <almir@kiberpipa.org>
2008-08-17 12:02:58 +0000

    some web page work, forgoten wiki admin.py
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@933 ddb1d346-470b-0410-b242-bdb7c8b48ed5

---
+++ intranet/wiki/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from models import *
+
+class CategoryAdmin(admin.ModelAdmin):
+    ordering = ('order',)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article)
+#admin.site.register(ChangeSet)
