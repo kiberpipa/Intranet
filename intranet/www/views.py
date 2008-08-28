fce4ff1888a9162714d7e1a4d5bcc30e5941cc95
Almir Karic <almir@kiberpipa.org>
2008-08-28 15:42:32 +0000

    webpage work, minor bugfix
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@963 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- intranet/www/views.py
+++ intranet/www/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render_to_response
 from intranet.org.models import Event
-# Create your views here.
+from django.template import RequestContext
+
+from intranet.feedjack.models import Post
+from intranet.photologue.models import Photo
 
 def index(request):
     last9 = Event.objects.all().order_by('-start_date')[0:8] 
-    #last3 = tmp[0:2]
-    #pre6 = tmp[:6]
     return render_to_response('www/index.html', {
         'last3': last9[0:3], 
         'pre6': last9[3:],
-        })
+        'planet': Post.objects.all().order_by('date_modified')[0:2],
+        'gallery': Photo.objects.all().order_by('date_added')[0:2],
+        },
+        context_instance=RequestContext(request))
