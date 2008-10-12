6d1397aa9af54927445c1557e3dfbbf74108150a
Almir Karic <almir@kiberpipa.org>
2008-10-12 11:24:05 +0000

    nadomestiu feedjackov f00 interface z generic view-om PS feedjack sucks
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1046 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- intranet/urls.py
+++ intranet/urls.py
@@ -3,11 +3,16 @@ from django.conf import settings
 from django.contrib import admin
 
 from intranet.photologue.models import *
+from intranet.feedjack.models import Post
 
 admin.autodiscover()
 
 SAMPLE_SIZE = ":%s" % getattr(settings, 'GALLERY_SAMPLE_SIZE', 5)
 gallery_args = {'date_field': 'date_added', 'allow_empty': True, 'queryset': Gallery.objects.filter(is_public=True), 'extra_context':{'sample_size':SAMPLE_SIZE}}
+
+planet_dict = {
+    'queryset': Post.objects.order_by('-date_created')[:30],
+}
 
 urlpatterns = patterns('',
     #(r'^video3/', include('intranet.video.urls')),
@@ -19,7 +24,8 @@ urlpatterns = patterns('',
     (r'^accounts/$', 'django.views.generic.simple.redirect_to', {'url': 'login/'}),
     (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
 
-    (r'^planet/', include('intranet.feedjack.urls')),
+    #(r'^planet/', include('intranet.feedjack.urls')),
+    (r'^planet/', 'django.views.generic.list_detail.object_list', planet_dict),
 
 
     (r'^gallery/', include('intranet.photologue.urls')),
