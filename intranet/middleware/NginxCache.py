d3ce39cc005934d56863d07f2d3a679ff67a3e2c
Almir Karic <almir@kiberpipa.org>
2008-11-01 01:22:50 +0000

    naredu novi caching feature developerjem prijazen
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1105 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- intranet/middleware/NginxCache.py
+++ intranet/middleware/NginxCache.py
@@ -3,18 +3,20 @@ import settings
 
 class NginxMemCacheMiddleWare:
     def process_response(self, request, response):
-        cacheIt = False
-        theUrl = request.get_full_path()
-
-        for exp in settings.CACHE_INCLUDE_REGEXPS:
-            if exp == theUrl:
-                cacheIt = True
-
-        # if it's a GET then store it in the cache:
-        if request.method != 'GET':
+        try:
             cacheIt = False
+            theUrl = request.get_full_path()
 
-        if cacheIt:
-            key = '%s-%s' % (settings.CACHE_KEY_PREFIX,theUrl)
-            cache.set(key,response.content)     
-        return response
+            for exp in settings.CACHE_INCLUDE_REGEXPS:
+                if exp == theUrl:
+                    cacheIt = True
+
+            # if it's a GET then store it in the cache:
+            if request.method != 'GET':
+                cacheIt = False
+
+            if cacheIt:
+                key = '%s-%s' % (settings.CACHE_KEY_PREFIX,theUrl)
+                cache.set(key,response.content)     
+        except AttributeError:
+            return response
