835b9da9cbdcc46c29dd5fc7fe6c35f0f16d2889
Almir Karic <almir@kiberpipa.org>
2008-11-30 20:37:12 +0000

    gandalfs patch to parse_videoarhiv.py
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1155 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- intranet/utils/parse_videoarhiv.py
+++ intranet/utils/parse_videoarhiv.py
@@ -2,19 +2,32 @@
 
 from BeautifulSoup import BeautifulSoup
 
+import urllib
+
 def parse_video(video):    
   results = {'thumb': video.find('img', {'class':'thumb'}).get('src', None),
              'title': video.find('a', {'class':'title'}).contents,
              'url': video.find('a', {'class':'title'}).get('href', None),
              'author': video.find('div', {'class':'videoinfo'}).findAll('p')[1].contents[0].split(' &bull;')[0],
              'date': video.find('div', {'class':'videoinfo'}).findAll('p')[1].contents[1].contents,
+             'intranet-id': None,
             }
+
+  #lets get intranet-id from info.txt now
+  
+  info_url = 'http://video.kiberpipa.org' + results.get('url')[:-9] + 'info.txt'
+  
+  f = urllib.urlopen(info_url)
+  
+  for line in f.readlines():
+    if 'intranet-id' in line:
+      results['intranet-id'] = line.split()[1]
   
   return results
 
 def main():
   
-  f = open('vse.html')
+  f = urllib.urlopen('http://video.kiberpipa.org/vse.html')
   soup = BeautifulSoup(f.read())
   f.close()
   
