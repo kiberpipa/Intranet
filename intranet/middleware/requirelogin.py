ca4b338ddc075c28e771315a38a8887795b6eb87
Gasper Zejn <hruske@kiberpipa.org>
2009-01-21 11:42:10 +0000

    Detabify.
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1191 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- intranet/middleware/requirelogin.py
+++ intranet/middleware/requirelogin.py
@@ -14,17 +14,17 @@ class RequireLoginMiddleware(object):
     page set by REQUIRE_LOGIN_PATH or /accounts/login/ by default.
     """
     def __init__(self):
-	    self.require_login_path = getattr(settings, 'REQUIRE_LOGIN_PATH', '/accounts/login/')
-	
+        self.require_login_path = getattr(settings, 'REQUIRE_LOGIN_PATH', '/accounts/login/')
+
     def process_request(self, request):
         excludes = ['/zavod/', '/intranet/feeds/', '/intranet/stats/text_log', '/smedia/', '/amedia/', '/']
         for i in excludes:
           if i in request.path:
             return None
-          
+        
         if request.path != self.require_login_path and request.user.is_anonymous():
             if request.POST:
                 return login(request)
             else:
                 return HttpResponseRedirect('%s?next=%s' % (self.require_login_path, request.path))
-                
+        
