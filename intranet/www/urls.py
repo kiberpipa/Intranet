8f5bd92697ed380178f7a10d78ecbfa0dbe36a22
Matjaz Crnko <matjaz.crnko@gmail.com>
2009-01-28 21:01:51 +0000

    More locale stuff, JS fallback
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1199 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- intranet/www/urls.py
+++ intranet/www/urls.py
@@ -11,12 +11,17 @@ urlpatterns = patterns('',
     url(r'^news/(?P<slug>[-\w]+)/?$', 'intranet.www.views.news'),
     url(r'^calendar/ical/?(?P<month>month)?/?$', 'intranet.www.views.ical'),
     url(r'^calendar/(?P<year>\d+)?/?(?P<month>\d+)?/?', 'intranet.www.views.calendar'),
-    #url(r'^calendar-en/', 'intranet.www.views.calendar', {'en': True}),
     url(r'^(modules|index)\.php', 'intranet.www.views.compat'),
     url(r'^alumni/', 'intranet.www.views.alumni'),
-    #url(r'^alumni-en/', 'django.views.generic.list_detail.object_list', alumni_dict),
     url(r'^press/', 'intranet.www.views.press'),
-    #url(r'^press-en/', 'django.views.generic.list_detail.object_list', press_dict_en),
+    url(r'^locale/?$', 'django.views.generic.simple.direct_to_template', {'template': 'www/locale.html'}),
     url(r'^ajax/gallery/(?P<id>\d+|[\w-]+)/$', 'intranet.www.views.gallery'),
     url(r'^ajax/index/events/$', 'intranet.www.views.ajax_index_events'),
 )
+
+# URL-ji potrebni redirecta po deployu
+#url(r'^press-en/', 'django.views.generic.list_detail.object_list', press_dict_en),
+#url(r'^alumni-en/', 'django.views.generic.list_detail.object_list', alumni_dict),
+#url(r'^calendar-en/', 'intranet.www.views.calendar', {'en': True}),
+
+
