de9d5a05635243b40e489c2af384019822453f0c
Almir Karic <almir@kiberpipa.org>
2009-02-18 12:10:12 +0000

    dodau language filed v News tko da ne errora vec ko gres na /en/news
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1211 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- intranet/www/models.py
+++ intranet/www/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from django.conf import settings
 
 from intranet.org.models import Event
 
@@ -16,6 +17,7 @@ class News(models.Model):
     author = models.ForeignKey(User)
     #the field for calendar id's which can not be matched by  title to stories
     calendar_id = models.IntegerField(blank=True, null=True)
+    language = models.CharField(max_length=2, default='si', choices=settings.LANGUAGES, blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('intranet.www.views.news', args=[self.slug])
