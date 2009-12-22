7f757957f8b911808d656f781e7b6d8c4d4138be
Gasper Zejn <hruske@kiberpipa.org>
2009-12-22 00:26:33 +0000

    DRY.
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1363 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- intranet/urls.py
+++ intranet/urls.py
@@ -53,7 +53,7 @@ urlpatterns = patterns('',
 if settings.DEBUG:
   from intranet.settings import next_to_this_file
   urlpatterns += patterns('',
-    (r'^smedia/(?P<path>.*)$', 'django.views.static.serve', {'document_root':   next_to_this_file(__file__, '../media')}),
+    (r'^smedia/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     #for shorter urls
     (r'^img/(?P<path>.*)$', 'django.views.static.serve', {'document_root':   next_to_this_file(__file__, '../media/photologue/photos')}),
     (r'^amedia/(?P<path>.*)$', 'django.views.static.serve', {'document_root':   next_to_this_file(__file__, '../admin-media')}),
