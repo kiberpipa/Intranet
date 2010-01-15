51f1bf6e226d9777b90281225c2ac0c8e34fc232
Gasper Zejn <hruske@kiberpipa.org>
2010-01-15 19:03:21 +0000

    move ldap auth backend
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1379 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- intranet/settings.py
+++ intranet/settings.py
@@ -129,7 +129,7 @@ FIXTURE_DIRS = (
 AUTH_PROFILE_MODULE = 'org.UserProfile'
 REQUIRE_LOGIN_PATH = '/intranet/accounts/login/'
 AUTHENTICATION_BACKENDS = (
-    'intranet.ldapbackend.LDAPAuthBackend',
+    'pipa.ldap.authbackend.LDAPAuthBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
 PHOTOLOGUE_DIR = 'photo/'
