52c48de5e3eed12f2b0b7036dc55db894e445dd7
Gasper Zejn <hruske@kiberpipa.org>
2010-01-15 19:48:19 +0000

    login required..
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1384 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- pipa/addressbook/views.py
+++ pipa/addressbook/views.py
@@ -1,15 +1,18 @@
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
 
 from pipa.addressbook.models import PipaProfile
 from pipa.addressbook.forms import ProfileForm
 
+@login_required
 def alumni(request):
 	profiles = PipaProfile.objects.filter(show_profile=True).order_by('user__first_name')
 	context = { 'profiles': profiles }
 	return render_to_response("alumni/alumni.html", RequestContext(request, context))
 
+@login_required
 def addressbook(request):
 	profile = request.user.get_profile()
 	profile_form = ProfileForm(instance=profile, initial={'email': request.user.email, 'first_name': request.user.first_name, 'last_name': request.user.last_name})
