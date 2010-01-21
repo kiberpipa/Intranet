dd533e083df80e54b2ba6626b34630a250a9872d
Gasper Zejn <hruske@kiberpipa.org>
2010-01-21 16:25:13 +0000

    Improved display
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1418 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- pipa/ltsp/models.py
+++ pipa/ltsp/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Usage(models.Model):
 	count = models.CommaSeparatedIntegerField(max_length=200)
 	time = models.DateTimeField()
+	
+	def __unicode__(self):
+		return u'LTSP usage: %s %s' % (self.time, self.count)
