c72ed2d385c62963b53246dee053b654d5f3d0ba
Gasper Zejn <hruske@kiberpipa.org>
2010-01-21 16:12:44 +0000

    LTSP stats yay
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1417 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- pipa/ltsp/src/ltsp_usage_monitor/ltsp_usage_daemon.py
+++ pipa/ltsp/src/ltsp_usage_monitor/ltsp_usage_daemon.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python
 
 import re
-from datetime import datetime
+import datetime
 import os
 import sys
 from glob import glob
 import pwd
 import socket
 import logging
+import md5
 import daemon
 import select
+import signal
 import stat
+import threading
+import time
+import simplejson as json
 
 ### Settings
 
 LTSP_HOME = '/home/ltsp'
 HOST=''
 PORT=31000
-DAEMON_CONF = '/home/hruske/projekti/pipa/kovchek/ltsp_usage_monitor/ltspstats.conf'
+DAEMON_CONF = os.path.join(os.path.dirname(__file__), 'ltspstats.conf')
 UNIX_SOCKET='/tmp/ltspstats'
+POST_INTERVAL = 60
+POST_URL = 'http://www.kiberpipa.org/services/ltsp/usage/'
+LTSP_USAGE_SECRET = 'xxx'
 
 M_AVG=6
 M_THR=3
@@ -30,9 +38,53 @@ log_re = re.compile("^\w+\s(?P<month>\w+)\s(?P<day>\d+)\s(?P<hours>\d+)\W(?P<min
 month = { 'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12,}
 
 
+def handle_exit(signum, frame):
+	print 'Signal received, stopping...'
+	for t in threading.enumerate():
+		if t.getName() != 'MainThread':
+			t.keep_running = False
+			t.join()
+	sys.exit(1)
+
+signal.signal(signal.SIGTERM, handle_exit)
+signal.signal(signal.SIGINT, handle_exit)
+
 def refresh():
 	uname = pwd.getpwuid(int(m.groups()[7]))
 	usage[uname.pw_name] = int(sum(term_list) > 3)
+
+class PostingThread(threading.Thread):
+	def __init__(self, statusdict):
+		self.keep_running = True
+		self.status = statusdict
+		super(PostingThread, self).__init__()
+	
+	def run(self):
+		import urllib
+		
+		while self.keep_running:
+			time.sleep(POST_INTERVAL)
+			
+			s = self.status.copy()
+			now = datetime.datetime.now() - datetime.timedelta(0, POST_INTERVAL)
+			terminals = {}
+			for k, v in s.iteritems():
+				vals = [i[1] for i in v if i[0] >= now]
+				if vals:
+					if sum(vals) >= M_THR:
+						terminals[k] = 1
+					else:
+						terminals[k] = 0
+			
+			data = json.dumps({'time': list(now.timetuple())[:7], 'count': sum(terminals.values())})
+			
+			post_args = {
+				'data': data,
+				'sign': md5.new(data + LTSP_USAGE_SECRET).hexdigest(),
+				}
+			
+			args = urllib.urlencode(post_args)
+			urllib.urlopen(POST_URL, args).read()
 
 class TerminalStatsDaemon(daemon.Daemon):
 	default_conf = DAEMON_CONF
@@ -41,6 +93,9 @@ class TerminalStatsDaemon(daemon.Daemon):
 	
 	def run(self):
 		status = {}
+		
+		pt = PostingThread(status)
+		pt.start()
 		
 		# cleanup unix socket
 		if os.path.exists(UNIX_SOCKET):
@@ -68,7 +123,7 @@ class TerminalStatsDaemon(daemon.Daemon):
 			inputready,outputready,exceptready = select.select(input,output,[])
 			for s in inputready:
 				# sprejemanje unix povezav
-				elif s == usocket:
+				if s == usocket:
 					uclient, address = usocket.accept()
 					logging.info(address)
 					input.append(uclient)
@@ -83,7 +138,7 @@ class TerminalStatsDaemon(daemon.Daemon):
 						lst = status.get(uname.pw_name,[])
 						
 						lst.append((
-							datetime(int(d['year']),int(month[d['month']]), int(d['day']),int(d['hours']),int(d['minutes']),int(d['seconds'])),
+							datetime.datetime(int(d['year']),int(month[d['month']]), int(d['day']),int(d['hours']),int(d['minutes']),int(d['seconds'])),
 							int(d['mousemoved']),
 							))
 						if len(lst) > M_AVG:
