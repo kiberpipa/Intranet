515cb7b1be0d0dcd6826750df7f871836c1b4752
Gasper Zejn <hruske@kiberpipa.org>
2010-01-22 15:04:16 +0000

    remove unused context processors
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1420 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- intranet/org/context_processors.py
+++ intranet/org/context_processors.py
@@ -5,20 +5,7 @@ def media_url(request):
         'media_url': settings.MEDIA_URL,
     }
 
-def base_url(request):
-    return {
-        'base_url': settings.BASE_URL,
-    }
-
 def admin_media_prefix(request):
     return {
         'admin_media_prefix': settings.ADMIN_MEDIA_PREFIX,
     }
-
-def sidebar_slug(request):
-    try:
-        current_slug = request.META['PATH_INFO']
-    except:
-        current_slug = ''
-    print current_slug
-    return {'current_slug': current_slug, }
