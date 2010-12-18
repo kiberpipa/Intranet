df06b32490d2ee012d9af2145d96c8bf17fbef50
Domen Kozar <ielectric@kiberpipa.org>
2010-12-18 01:12:44 +0000

    update dependencies
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1546 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 import os
 
 from setuptools import setup, find_packages
@@ -35,11 +34,12 @@ setup(name='kiberpipa-intranet',
         # wiki
         'markdown2',
         # django stuff
-        'Django>=1.2',
+        'Django==1.2.3',
         'django-localeurl',
-        'tagging',
+        'django-tagging==0.3.1',
         'South',
         'flickrapi',
+        'django-syncr',
     ],
     tests_require=[
         'nose',
