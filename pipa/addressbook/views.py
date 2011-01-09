30b7003301789a043ca701a0567d14b2f0972e51
Domen Kozar <ielectric@kiberpipa.org>
2011-01-09 17:15:52 +0000

    cosmetic stuff
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1558 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- pipa/addressbook/views.py
+++ pipa/addressbook/views.py
@@ -8,10 +8,10 @@ from django.contrib.auth.models import User
 from pipa.addressbook.models import PipaProfile
 from pipa.addressbook.forms import ProfileForm
 
+
 def alumni(request):
 	profiles = PipaProfile.objects.filter(show_profile=True).order_by('user__first_name')
-	context = { 'profiles': profiles }
-	return render_to_response("alumni/alumni.html", RequestContext(request, context))
+	return render_to_response("alumni/alumni.html", RequestContext(request, locals()))
 
 @login_required
 def addressbook(request):
