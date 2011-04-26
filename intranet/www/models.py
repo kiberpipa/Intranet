8038ce849f33533e43d1f9c1b3de906e5a67639c
Domen Kozar <ielectric@kiberpipa.org>
2011-04-26 09:21:12 +0000

    semantics
    git-svn-id: https://www.kiberpipa.org/svn/intranet/rd666@1586 ddb1d346-470b-0410-b242-bdb7c8b48ed5

--- intranet/www/models.py
+++ intranet/www/models.py
@@ -1,13 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.conf import settings
 
-from intranet.org.models import Event
 
-# Create your models here.
-
-#blog posts
 class News(models.Model):
     title = models.CharField(max_length=150)
     text = models.TextField()
@@ -15,7 +14,7 @@ class News(models.Model):
     date = models.DateTimeField(auto_now_add=True, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
     author = models.ForeignKey(User)
-    #the field for calendar id's which can not be matched by  title to stories
+    #the field for calendar id's which can not be matched by title to stories
     calendar_id = models.IntegerField(blank=True, null=True)
     language = models.CharField(max_length=2, default='si', choices=settings.LANGUAGES, blank=True, null=True)
 
@@ -25,6 +24,7 @@ class News(models.Model):
     def __unicode__(self):
         return self.title
 
+
 class Ticker(models.Model):
     name = models.CharField(max_length=100)
     note = models.TextField()
@@ -32,4 +32,3 @@ class Ticker(models.Model):
 
     def __unicode__(self):
         return self.name
-
