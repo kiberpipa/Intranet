d3bb99dc3b7853927c1c0d8a7cffc841110d7186
Domen Kožar <domen@dev.si>
2011-07-18 19:04:31 +0200

    pep

--- intranet/org/forms.py
+++ intranet/org/forms.py
@@ -10,10 +10,10 @@ from django.contrib.auth.models import User
 from django.utils.encoding import force_unicode
 from django.conf import settings
 from django.forms.util import ErrorList
-from django.forms.models import ModelChoiceField, ModelMultipleChoiceField
+from django.forms.models import ModelChoiceField
 
-from intranet.org.models import (TipSodelovanja, Person, Event, Sodelovanje,
-    Project, Category, Lend, Diary, Shopping, IntranetImage, EmailBlacklist, User)
+from intranet.org.models import (Person, Event, Sodelovanje,
+    Project, Lend, Diary, Shopping, IntranetImage, EmailBlacklist)
 
 # DATETIMEWIDGET
 # TODO: FUUUUUUUUcked up.
@@ -31,21 +31,23 @@ calbtn = u"""<img src="/static/org/images/calendar.jpg" alt="calendar" id="%s_bt
 
 class DateTimeWidget(forms.widgets.TextInput):
     dformat = '%Y-%m-%d %H:%M'
+
     def render(self, name, value, attrs=None):
-        if value is None: value = ''
+        if value is None:
+            value = ''
         final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
-        if value != '': 
+        if value != '':
             try:
                 final_attrs['value'] = \
                                    force_unicode(value.strftime(self.dformat))
             except:
                 final_attrs['value'] = \
                                    force_unicode(value)
-        if not final_attrs.has_key('id'):
+        if 'id' not in final_attrs:
             final_attrs['id'] = u'%s_id' % (name)
         id = final_attrs['id']
 
-        jsdformat = self.dformat #.replace('%', '%%')
+        jsdformat = self.dformat  # .replace('%', '%%')
         cal = calbtn % (id, id, jsdformat, id)
         a = u'<input%s />%s' % (forms.util.flatatt(final_attrs), cal)
         return a
@@ -68,9 +70,11 @@ class DateTimeWidget(forms.widgets.TextInput):
                 continue
         return None
 
+
 class DiaryFilter(forms.Form):
     task = forms.ModelChoiceField(Project.objects.all().order_by('name'), required=False)
     author = forms.ModelChoiceField(User.objects.filter(is_active=True).order_by('username'), required=False)
+
 
 class PersonForm(forms.Form):
     name = forms.CharField(max_length=200)
@@ -79,35 +83,41 @@ class PersonForm(forms.Form):
     organization = forms.CharField(max_length=200, required=False)
     title = forms.CharField(max_length=200, required=False)
 
+
 class AddEventEmails(forms.Form):
-    emails = forms.CharField(widget=forms.Textarea(attrs={'cols':'31'}))
+    emails = forms.CharField(widget=forms.Textarea(attrs={'cols': '31'}))
+
 
 class CommaSeparatedIntegerField(forms.CharField):
     def clean(self, value):
         values = value.strip().split(',')
         for i in values:
             if not re.match('^\d+$', i):
-                raise ValidationError("Integer is required.")
+                raise forms.ValidationError("Integer is required.")
         return [int(i) for i in values]
+
 
 class ImageResizeForm(forms.Form):
     resize = CommaSeparatedIntegerField(widget=forms.HiddenInput)
     filename = forms.CharField(widget=forms.HiddenInput)
 
+
 class EmailBlacklistForm(forms.ModelForm):
     class Meta:
         model = EmailBlacklist
+
 
 class IntranetImageForm(forms.ModelForm):
     class Meta:
         model = IntranetImage
         exclude = ('md5',)
 
+
 class EventForm(forms.ModelForm):
     start_date = forms.DateTimeField(label=u"Pričetek", widget=DateTimeWidget)
     end_date = forms.DateTimeField(label=u"Zaključek", widget=DateTimeWidget, required=False)
     title = forms.CharField(label="Naslov", max_length=Event._meta.get_field('title').max_length,
-        widget=forms.TextInput(attrs={'size':'60'}))
+        widget=forms.TextInput(attrs={'size': '60'}))
     responsible = forms.CharField(label="Odgovorna oseba")
 
     class Meta:
@@ -140,7 +150,7 @@ class EventForm(forms.ModelForm):
         cleaned_data = self.cleaned_data
         public = cleaned_data.get("public")
         if public:
-            if not ( cleaned_data.get("event_image") or self.instance.sequence > 0 ):
+            if not (cleaned_data.get("event_image") or self.instance.sequence > 0):
                 self._errors["event_image"] = ErrorList(['Javni dogodki potrebujejo sliko.'])
             if not cleaned_data.get("announce"):
                 self._errors["announce"] = ErrorList([u'Javni dogodki potrebujejo najavo.'])
@@ -158,22 +168,24 @@ class SodelovanjeFilter(forms.ModelForm):
         model = Sodelovanje
         exclude = ('note',)
 
+
 class LendForm(forms.ModelForm):
     from_who = ModelChoiceField(User.objects.filter(is_active=True).order_by('username'))
 
     class Meta:
         model = Lend
-        fields = ('what','to_who', 'from_who', 'contact_info', 'due_date',)
+        fields = ('what', 'to_who', 'from_who', 'contact_info', 'due_date',)
+
 
 class ShoppingForm(forms.ModelForm):
     class Meta:
         model = Shopping
         fields = ('name', 'explanation', 'cost', 'project', )
 
+
 class DiaryForm(forms.ModelForm):
     task = ModelChoiceField(Project.objects.all().order_by('name'))
 
     class Meta:
         model = Diary
-        fields = ('task', 'date', 'length', 'log_formal', 'log_informal',)
-
+        fields = ('task', 'date', 'length', 'log_formal', 'log_informal')
