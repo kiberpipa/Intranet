0134058dde2a43b927f7e7628871c31760f56d5c
Domen Kožar <domen@dev.si>
2011-09-01 14:19:14 +0200

    dbdumper superseeded by fabric

--- dbdumper/dump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PYTHONPATH=`pwd`/..:`pwd`/../libs:`pwd`/../intranet ./db_dump.py -v --settings=intranet.settings -d dbdump/ dump
-
-tar jcvf ~/dbdump.tar.bz2 dbdump
-
-rm -rf dbdump
--- dbdumper/load.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-APPS="contenttypes auth www org tags wiki sites flatpages admin markup"
-
-PYTHONPATH=`pwd`/../libs:`pwd`/.. ./db_dump.py -v --settings=intranet.settings -d dbdump load $APPS
