d0174c75129b0abf14ead4685234aba6672d252d
Domen Kožar <domen@dev.si>
2011-09-10 18:18:05 +0200

    implement flickr caching and better error handling

--- pipa/gallery/templatetags/photos_box.py
+++ pipa/gallery/templatetags/photos_box.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import logging
+
 import flickrapi
+from django.core.cache import cache
 from django.conf import settings
 from django.utils import simplejson
 from django.template import Node, Library, loader
 
+
+logger = logging.getLogger(__name__)
 register = Library()
+
+api = flickrapi.FlickrAPI(
+        settings.PHOTOS_FLICKR_APIKEY,
+        settings.PHOTOS_FLICKR_SECRET,
+        token=settings.PHOTOS_FLICKR_TOKEN,
+        cache=True,
+        )
+api.cache = cache
 
 
 class PhotosBoxNode(Node):
@@ -18,12 +31,6 @@ class PhotosBoxNode(Node):
         if not flickr_set_id:
             return ''
 
-        api = flickrapi.FlickrAPI(
-                settings.PHOTOS_FLICKR_APIKEY,
-                settings.PHOTOS_FLICKR_SECRET,
-                token=settings.PHOTOS_FLICKR_TOKEN,
-                )
-
         r = api.flickr_call(
                 method='flickr.photosets.getPhotos',
                 photoset_id=flickr_set_id,
@@ -32,14 +39,19 @@ class PhotosBoxNode(Node):
 
         images = []
         json = simplejson.loads(r)
-        for image in json['photoset']['photo']:
-            image['thumb_url'] = settings.PHOTOS_FLICKR_IMAGE_URL_S % image
-            image['url'] = settings.PHOTOS_FLICKR_IMAGE_URL % image
-            image['title'] = image.get('title', '')
-            images.append(image)
-        t = loader.get_template('gallery/photos_box.html')
-        context['images'] = images
-        return t.render(context)
+
+        if json.get('stat', 'error') == 'ok':
+            for image in json['photoset']['photo']:
+                image['thumb_url'] = settings.PHOTOS_FLICKR_IMAGE_URL_S % image
+                image['url'] = settings.PHOTOS_FLICKR_IMAGE_URL % image
+                image['title'] = image.get('title', '')
+                images.append(image)
+            t = loader.get_template('gallery/photos_box.html')
+            context['images'] = images
+            return t.render(context)
+        else:
+            logger.error('Could not fetch images from Flickr', exc_info=True, extra=locals())
+            return ''
 
 
 def photos_box(parser, token):
