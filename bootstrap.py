b32ec6ef4a6061d19643c75f0e744b1ecebf5175
Domen Kožar <domen@dev.si>
2013-02-10 20:45:16 +0100

    pin buildout to 1.7.0

--- bootstrap.py
+++ bootstrap.py
@@ -101,7 +101,7 @@ local resources, you can keep this script from going over the network.
 '''
 
 parser = OptionParser(usage=usage)
-parser.add_option("-v", "--version", dest="version",
+parser.add_option("-v", "--version", dest="version", default="1.7.0",
                           help="use a specific zc.buildout version")
 parser.add_option("-d", "--distribute",
                    action="store_true", dest="use_distribute", default=False,
